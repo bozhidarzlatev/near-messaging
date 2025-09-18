feb3df797db8c30cb323264c53ca827616fa7ff5
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2025-09-18 19:35:53 +0300

    signhup code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-        try {
-            const conn = await mongoose.connect(process.env.MONGO_URI, {
-                dbName: "near_messaging"
-            });
-            console.log("MongoDB Connected!", conn.connection.host);
-        } catch (error) {
-            console.error("Error connection to MongoDB:" , error);
-            process.exit(1);            
-        }
+    try {
+        const { MONGO_URI } = process.env;
+        if (!MONGO_URI) throw new Error("MONGO_URI is not set!");
+        
+
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "near_messaging"
+        });
+        console.log("MongoDB Connected!", conn.connection.host);
+    } catch (error) {
+        console.error("Error connection to MongoDB:", error);
+        process.exit(1);
+    }
 }
