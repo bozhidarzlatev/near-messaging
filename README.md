763184cc7bae82f548a8fb29010774841c50d42b
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2025-09-18 18:10:33 +0300

    MongoDB added

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 Messaging app with real time conversation functionalities.
 
 
+[Visit Project](https://near-messaging.sevalla.app/ "Have fun!")
+
 Stack:
     - express.js for backend
     - sevalla for code deployment
-    
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from './lib/db.js';
 
 dotenv.config()
 
@@ -21,4 +22,7 @@ if(process.env.NODE_ENV === "production") {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
     })
 }
-app.listen(3000, () => console.log(`Server is running on port ${PORT}!`));
+app.listen(3000, () => {
+    console.log(`Server is running on port ${PORT}!`);
+    connectDB();
+});
