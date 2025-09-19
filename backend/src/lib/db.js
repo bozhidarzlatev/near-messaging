446115b7a1163147075fecc645aa449e812cc591
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2025-09-19 14:20:54 +0300

    login and logout func added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,9 +3,9 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = ENV;
+        const {MONGO_URI } = ENV;
+            
         if (!MONGO_URI) throw new Error("MONGO_URI is not set!");
-        
 
         const conn = await mongoose.connect(ENV.MONGO_URI, {
             dbName: "near_messaging"
